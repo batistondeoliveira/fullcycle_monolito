a4bcb4b7689fc4a8364f998b55c336793f98cfc7
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-29 22:03:21 -0300

    feat: finalizando StoreCatalogFacade

---
+++ src/modules/store-catalog/factory/store-catalog.facade.factory.ts
@@ -0,0 +1,17 @@
+import ProductRepository from "../repository/product.repository";
+import FindAllProductUseCase from "../usecase/find-all-products/find-all-products.usecase";
+import FindProductUseCase from "../usecase/find-product/find-product.usecase";
+
+export default class StoreCatalogFacadeFactory {
+  static create(): StoreCatalogFacade {
+    const productRepository = new ProductRepository();
+    const findUseCase = new FindProductUseCase(productRepository);
+    const findAllUseCase = new FindAllProductUseCase(productRepository);
+
+    const facade = new StoreCatalogFacade({
+      findUseCase: findUseCase,
+      findAllUseCase: findAllUseCase,
+    });
+    return facade;
+  }
+}
