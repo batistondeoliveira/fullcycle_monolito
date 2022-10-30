062f9d9636a2f507c8b989ad4716333d1d579974
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-29 21:29:18 -0300

    feat: implementando findAll no ProductRepository

--- src/modules/store-catalog/repository/product.repository.spec.ts
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "./product.model";
+import ProductRepository from "./product.repository";
 
 describe("ProductRepository test", () => {
   let sequelize: Sequelize;
@@ -39,11 +40,11 @@ describe("ProductRepository test", () => {
     const products = await productRepository.findAll();
     
     expect(products.length).toBe(2);
-    expect(products[0].id).toBe("1");
+    expect(products[0].id.id).toBe("1");
     expect(products[0].name).toBe("Product 1");
     expect(products[0].description).toBe("Description 1");
     expect(products[0].salesPrice).toBe(100);
-    expect(products[1].id).toBe("2");
+    expect(products[1].id.id).toBe("2");
     expect(products[1].name).toBe("Product 2");
     expect(products[1].description).toBe("Description 2");
     expect(products[1].salesPrice).toBe(200);
