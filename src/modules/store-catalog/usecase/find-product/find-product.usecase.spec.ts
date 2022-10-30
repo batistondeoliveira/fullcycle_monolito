9dd03f5415356c8430c11b7a8499629ce7611212
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-29 21:45:59 -0300

    test: finalizando FindProductUseCase

--- src/modules/store-catalog/usecase/find-product/find-product.usecase.spec.ts
+++ src/modules/store-catalog/usecase/find-product/find-product.usecase.spec.ts
@@ -1,6 +1,7 @@
 import Id from "../../../@shared/domain/value-object/id.value-object";
 import Product from "../../domain/product.entity";
 import FindAllProductUseCase from "../find-all-products/find-all-products.usecase";
+import FindProductUseCase from "./find-product.usecase";
 
 const product = new Product({
   id: new Id("1"),
