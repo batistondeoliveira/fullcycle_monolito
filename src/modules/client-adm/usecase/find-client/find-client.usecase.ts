3b5e3c39a658008a723896df74959a5abb2af771
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-30 09:57:48 -0300

    feat: implementando FindClientUseCase

---
+++ src/modules/client-adm/usecase/find-client/find-client.usecase.ts
@@ -0,0 +1,25 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Client from "../../domain/client.entity";
+import ClientAdmGateway from "../../gateway/client-adm.gateway";
+import { FindClientInputDto, FindClientOutputDto } from "./find-client.usecase.dto";
+
+export default class FindClientUseCase {
+  private _clientRepository: ClientAdmGateway;
+  
+  constructor(clientRepository: ClientAdmGateway) {
+    this._clientRepository = clientRepository;
+  }
+
+  async execute(input: FindClientInputDto): Promise<FindClientOutputDto> {    
+    const result = await this._clientRepository.find(input.id);
+    
+    return {
+      id: result.id.id,
+      name: result.name,
+      email: result.email,
+      address: result.address, 
+      createdAt: result.createdAt,
+      updatedAt: result.updatedAt,
+    };
+  }
+}
