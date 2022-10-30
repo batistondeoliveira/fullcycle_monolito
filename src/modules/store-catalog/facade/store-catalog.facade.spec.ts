3979c7a8f968bc68a7e87ba2171bd3b797287a3a
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-29 22:03:26 -0300

    test: finalizando StoreCatalogFacade

---
+++ src/modules/store-catalog/facade/store-catalog.facade.spec.ts
@@ -0,0 +1,40 @@
+import { Sequelize } from "sequelize-typescript";
+import StoreCatalogFacadeFactory from "../factory/store-catalog.facade.factory";
+import ProductModel from "../repository/product.model";
+
+describe("StoreCatalogFacade test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find a product", async () => {
+    const facade = StoreCatalogFacadeFactory.create();
+
+    await ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Description 1",
+      salePrice: 100,
+    });
+
+    const result = await facade.find({ id: "1" });
+    expect(result.id).toBe("1");
+    expect(result.name).toBe("Product 1");
+    expect(result.description).toBe("Description 1");
+    expect(result.salesPrice).toBe(100);
+  });
+});
