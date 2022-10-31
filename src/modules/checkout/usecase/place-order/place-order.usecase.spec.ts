095f10c670f1db4dad9e1ad72fd1a4fbdcdb9c8b
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-31 08:54:48 -0300

    test: testando validacao dos produtos

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -21,5 +21,33 @@ describe("PlaceOrderUseCase unit test", () => {
         new Error("Client not found")
       );
     });
+
+    it("should throw an error when products are not valid", async () => {
+      const mockClientFacade = {
+        find: jest.fn().mockReturnValue(null),
+      };
+      //@ts-expect-error - no params in constructor
+      const placeOrderUseCase = new PlaceOrderUseCase();
+
+      const mockValidateProducts = jest
+        //@ts-expect-error - spy on private method
+        .spyOn(placeOrderUseCase, "validateProducts")
+        //@ts-expect-error - not return never
+        .mockRejectValue(new Error("No products selected"));
+
+      //@ts-expect-error - force set clientFacade
+      placeOrderUseCase["_clientFacade"] = mockClientFacade;
+
+      const input: PlaceOrderInputDto = {
+        clientId: "1",
+        products: []
+      };
+
+      await expect(placeOrderUseCase.execute(input)).rejects.toThrow(
+        new Error("No products selected")
+      );
+      expect(mockValidateProducts).toHaveBeenCalledTimes(1);
+    });
+
   });
 });
