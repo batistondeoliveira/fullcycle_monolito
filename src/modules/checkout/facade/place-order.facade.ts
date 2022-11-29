c70571160479cdd01215d21cf23ac071cb7822ed
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-11-29 09:20:06 -0300

    feat: Desafio Criacao API - adicionado crud place order

---
+++ src/modules/checkout/facade/place-order.facade.ts
@@ -0,0 +1,60 @@
+import UseCaseInterface from "../../@shared/usecase/use-case.interface";
+import { FindPlaceOrderFacadeInputDto, FindPlaceOrderFacadeOutputDto, PlaceOrderFacadeInputDto, PlaceOrderFacadeOutputDto } from "./place-order.facade.dto";
+import PlaceOrderFacadeInterface from "./place-order.facade.interface";
+
+export interface UseCaseProps {
+  findUsecase: UseCaseInterface,
+  addUsecase: UseCaseInterface,
+}
+
+export default class PlaceOrderFacade implements PlaceOrderFacadeInterface {
+  private _findUsecase: UseCaseInterface;
+  private _addUsecase: UseCaseInterface;
+
+  constructor(usecaseProps: UseCaseProps) {
+    this._addUsecase = usecaseProps.addUsecase;
+    this._findUsecase = usecaseProps.findUsecase;
+  }
+
+  async add(input: PlaceOrderFacadeInputDto): Promise<PlaceOrderFacadeOutputDto> {
+    const placeOrderInputDto = { 
+      id: input.id,     
+      clientId: input.clientId,
+      products: input.products.map(items => ({
+        productId: items.productId
+      })),
+    };
+
+    const usecase = await this._addUsecase.execute(placeOrderInputDto);    
+
+    return {
+      id: usecase.id,
+      invoiceId: usecase.invoiceId,
+      status: usecase.status,
+      total: usecase.total,
+      products: usecase.products,
+    };
+  }
+
+  async find(
+    input: FindPlaceOrderFacadeInputDto
+  ): Promise<FindPlaceOrderFacadeOutputDto> {
+    const usecase = await this._findUsecase.execute(input);
+
+    return {
+      id: usecase.id,      
+      name: usecase.client.name,      
+      email: usecase.client.email,
+      document: usecase.client.document,
+      address: usecase.client.address,
+      items: usecase.products.map((product: any) => ({
+        id: product.id.id,        
+        name: product.name,
+        description: product.description,
+        salesPrice: product.salesPrice,
+      })),      
+      status: usecase.status,
+      createdAt: usecase.createdAt,
+    };
+  }
+}
